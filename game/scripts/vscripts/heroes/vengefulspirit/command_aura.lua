0581619cc86c813653ae66883466e3626f51a36a
Trent Monahan <github@361zn.is>
2017-11-27 00:36:35 +1100

    Added branding and version info to files

--- game/scripts/vscripts/heroes/vengefulspirit/command_aura.lua
+++ game/scripts/vscripts/heroes/vengefulspirit/command_aura.lua
@@ -1,3 +1,10 @@
+--[[
+ModDota AbilityLuaSpellLibrary implementation for Vengenge Aura
+
+Mechanics correct as at 7.07c (excluding talents)
+See github for contributor list
+--]]
+
 LinkLuaModifier("modifier_vengefulspirit_command_aura_effect_lua", "heroes/vengefulspirit/command_aura", LUA_MODIFIER_MOTION_NONE)
 LinkLuaModifier("modifier_vengefulspirit_command_aura_lua", "heroes/vengefulspirit/command_aura", LUA_MODIFIER_MOTION_NONE)
 
--- game/scripts/vscripts/heroes/vengefulspirit/nether_swap.lua
+++ game/scripts/vscripts/heroes/vengefulspirit/nether_swap.lua
@@ -1,3 +1,10 @@
+--[[
+ModDota AbilityLuaSpellLibrary implementation for Nether Swap
+
+Mechanics correct as at 7.07c
+See github for contributor list
+--]]
+
 ---@class vengeful_nether_swap_lua : CDOTA_Ability_Lua
 vengefulspirit_nether_swap_lua = class({})
 
